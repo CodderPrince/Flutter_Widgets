8b04505020eed0ad0553cc799529ed671908f1f5
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-28 00:42:45 +0600

    Create MyAppBar21.dart

---
+++ ListView/MyAppBar21.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget MyAppBar21() {
+  return AppBar(
+    elevation: 8,
+    backgroundColor: Colors.transparent,
+    flexibleSpace: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(107, 48, 0, 1.0),
+            Colors.lightGreen,
+            Colors.blueGrey,
+            Colors.teal,
+            Colors.deepPurple,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(0)),
+        boxShadow: [
+          BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(0, 4)),
+        ],
+      ),
+    ),
+
+    title: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(width: 8),
+        ShaderMask(
+          shaderCallback:
+              (bounds) => const LinearGradient(
+                colors: [Colors.yellow, Colors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+          child: const Text(
+            "ListView",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+    centerTitle: true,
+  );
+}
