c5ecb1bb84e88806ad54aa34b48243e887e6e345
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-28 00:22:34 +0600

    Create textStyle20.dart

---
+++ GridView/textStyle20.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+TextStyle textStyle20 = TextStyle(
+  color: Colors.purple, // Custom color
+  fontSize: 24, // Custom font size
+  fontWeight: FontWeight.bold, // Custom font weight
+  fontStyle: FontStyle.italic, // Custom font style
+);
