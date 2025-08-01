d2902e5d48ccd599c11636132bbdf674e40855c0
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-01 20:32:22 +0600

    Create main1.dart

---
+++ Basic3/lib/main1.dart
@@ -0,0 +1,33 @@
+import 'package:basic3/myAppbar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.amber),
+      color: Colors.blue,
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppbar(title: 'called appbar!!',),
+      body: Center(child: Text("hello prince",style: TextStyle(color: Colors.blue),)),
+    );
+  }
+}
